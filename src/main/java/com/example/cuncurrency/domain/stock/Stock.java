2e85860fdccbed5b190556da11c490a3d29a2c77
yongckim <zxcv9203@naver.com>
2023-01-17 22:59:32 +0900

    Race Condition 발생시키기

--- src/main/java/com/example/cuncurrency/domain/stock/Stock.java
+++ src/main/java/com/example/cuncurrency/domain/stock/Stock.java
@@ -10,12 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Getter
+@ToString
 public class Stock {
 
     @Id
--- src/test/java/com/example/cuncurrency/service/StockServiceTest.java
+++ src/test/java/com/example/cuncurrency/service/StockServiceTest.java
@@ -5,6 +5,10 @@ import com.example.cuncurrency.domain.stock.Stock;
 import com.example.cuncurrency.repository.StockRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Transactional
 class StockServiceTest {
     @Autowired
     private StockService stockService;
@@ -24,13 +27,18 @@ class StockServiceTest {
     private Stock stock;
 
     @BeforeEach
-    public void before() {
+    void before() {
         stock = Stock.builder()
             .productId(1L)
             .quantity(100L)
             .build();
 
         stock = stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    void after() {
+        stockRepository.deleteAllInBatch();
     }
 
     @Test
@@ -42,6 +50,42 @@ class StockServiceTest {
 
         // when
         stockService.decrease(stock.getId(), decreaseCount);
+        Stock got = stockRepository.findById(stock.getId())
+            .orElseThrow();
+
+        // then
+        assertThat(got.getQuantity()).isEqualTo(want);
+    }
+
+    /**
+     * ! Race Condition 발생
+     * ? ExecutorService에 대해서
+     * ? CountDownLatch에 대해서
+     */
+    @Test
+    @DisplayName("동시에 100개의 요청이 들어오는 요청")
+    void stock_decrease_100_request() throws InterruptedException {
+        // given
+        Long want = 0L;
+        Long decreaseCount = 1L;
+        int threadCount = 100;
+        // ? 100개의 요청을 날리기 위해 사용
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // ? 100개의 요청이 끝날때까지 기다리는 용도
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        System.out.println(stock.getId());
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(stock.getId(), decreaseCount);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
         Stock got = stockRepository.findById(stock.getId())
             .orElseThrow();
 
