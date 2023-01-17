d4a35d462bf65f8d7cf48229ac9c03930d1e93e6
yongckim <zxcv9203@naver.com>
2023-01-17 22:19:47 +0900

    프로젝트 초기화

---
+++ src/main/java/com/example/cuncurrency/CuncurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuncurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuncurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuncurrencyApplication.class, args);
+    }
+
+}
